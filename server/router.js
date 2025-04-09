325845c48e33360f36f0413602a48235a363f675
katie-b9030 <klb9030@rit.edu>
2025-04-09 12:37:13 -0400

    had to re-copy after creating p2 repository

--- server/router.js
+++ server/router.js
@@ -1,43 +1,43 @@
-const controllers = require("./controllers");
-const mid = require("./middleware");
+const controllers = require('./controllers');
+const mid = require('./middleware');
 
 const router = (app) => {
   app.get(
-    "/login",
+    '/login',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.loginPage
+    controllers.Account.loginPage,
   );
   app.post(
-    "/login",
+    '/login',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.login
+    controllers.Account.login,
   );
 
   app.get(
-    "/signup",
+    '/signup',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.signupPage
+    controllers.Account.signupPage,
   );
   app.post(
-    "/signup",
+    '/signup',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.signup
+    controllers.Account.signup,
   );
 
-  app.get("/logout", mid.requiresLogin, controllers.Account.logout);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
-  app.get("/maker", mid.requiresLogin, controllers.Domo.makerPage);
-  app.post("/maker", mid.requiresLogin, controllers.Domo.makeDomo);
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
   app.get(
-    "/",
+    '/',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.loginPage
+    controllers.Account.loginPage,
   );
 };
 
